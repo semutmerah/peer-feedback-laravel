0f40992827bc81f21665cfecd04b3cf3bfd2cdf4
Mufti <dev-null@prehistoric.me>
2015-12-15 04:35:25 +0000

    spesifik label & input pada review entry cept

--- tests/acceptance/ReviewEntryCept.php
+++ tests/acceptance/ReviewEntryCept.php
@@ -2,7 +2,9 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('perform submit feedback for other programmer');
 $I->amOnPage('/feedback/programmer-slug-name/new');
+$I->seeElement('img[alt="Logo KMK"]');
 $I->see('Developer Peer Feedback');
-$I->seeElement('label[id="pertanyaan_1"]');
-$I->seeElement('input', ['name' => 'jawaban[]']);
-
+$I->seeElement('label', ['id' => 'pertanyaan_1', 'for' => 'jawaban_1']);
+$I->seeElement('input', ['name' => 'jawaban[]', 'id' => 'jawaban_1']);
+$I->seeElement('label', ['id' => 'pertanyaan_2', 'for' => 'jawaban_2']);
+$I->seeElement('input', ['name' => 'jawabanp[]', 'id' => 'jawaban_2']);
