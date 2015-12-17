5a441db978fe36a882b1405a87dede286b304587
semutmerah <rasyid@rasyidfahroni.com>
2015-12-17 14:51:35 +0700

    left right keypress for peer summaries :sunglasses:

--- resources/views/partials/_body.blade.php
+++ resources/views/partials/_body.blade.php
@@ -7,4 +7,5 @@
 
 	<!-- Application JS-->
 	<script src="/js/all.js"></script>
+	<script src="/js/custom.js"></script>
 </body>
--- resources/views/summarise.blade.php
+++ resources/views/summarise.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 <div class="section">
-	<form>
+	<form action="#" method="post">
 		<h1 class="section-title" id="section-1">Konteks</h1>
 		<div class="section-content">
-			<p class="question" id="question">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor odio sit amet commodo efficitur. Sed posuere arcu dui, consequat pulvinar sem mollis et. Aliquam eu suscipit sem, eu consectetur elit. Maecenas id tellus ac enim auctor molestie.</p>
+			<p class="question" id="question-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer auctor odio sit amet commodo efficitur. Sed posuere arcu dui, consequat pulvinar sem mollis et. Aliquam eu suscipit sem, eu consectetur elit. Maecenas id tellus ac enim auctor molestie.</p>
 				<table>
 					<tr>
 					 <td>See Review From: </td>
 					 <td>
-						<a href="#" id="reviewer-1" class="reviewer">Tommy</a>
+						<a href="#" id="reviewer-1" class="reviewer active">Tommy</a>
 						<a href="#" id="reviewer-2" class="reviewer">Mohan</a>
 						<a href="#" id="reviewer-3" class="reviewer">Denny</a>
 						<a href="#" id="reviewer-4" class="reviewer">Indra</a>
@@ -19,7 +19,12 @@
 					 </td>
 					</tr>
 				</table>
-			<p class="feedback" id="feedback-1">Lorem ipsum doloir sit amet, consectetur adipiscing elit. Integer auctor odio sit amet commodo efficitur. Sed posuere arcu dui, consequat pulvinar sem mollis et. Aliquam eu suscipit sem, eu consectetur elit. Maecenas id tellus ac enim auctor molestie.</p>
+			<p class="feedback show" id="feedback-1">Lorem ipsum doloir feedback pertama</p>
+			<p class="feedback hide" id="feedback-2">Lorem ipsum doloir feedback kedua</p>
+			<p class="feedback hide" id="feedback-3">Lorem ipsum doloir feedback ketiga</p>
+			<p class="feedback hide" id="feedback-4">Lorem ipsum doloir feedback keempat</p>
+			<p class="feedback hide" id="feedback-5">Lorem ipsum doloir feedback kelima</p>
+			<p class="feedback hide" id="feedback-6">Lorem ipsum doloir feedback keenam</p>
 			<p class="summary-label">Summary: </p>
 			<textarea rows="4" id="summary-1" class="summary"></textarea>
 		</div>
