f9e667685a38227c1be61d153baaf2b114fbe9d6
Mufti <dev-null@prehistoric.me>
2015-12-18 03:25:32 +0000

    fix bug sebelumnya, belum dites juga :pensive:

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -1,38 +1,32 @@
 $(document).ready(function() {
 	var feedback_total = $(".feedback-set.show .feedback").length;
 	$("body").keydown(function (e){
-        /*
-		var reviewer = $(".reviewer.active");
-		var regex = /.*-(\d+)/;*/
-		var answer = $("feedback-set.show .feedback.show");
+		var reviewer = $(".feedback-set.show .reviewer.active");
+		var answer = $(".feedback-set.show .feedback.show");
 		var id_now_number = $(answer).index('.feedback-set.show .feedback');
+
+        $(answer).removeClass("show");
+        $(answer).addClass("hide");
+        $(reviewer).removeClass("active");
+
 		if (e.keyCode == 37) { //left
-		$(answer).removeClass("show");
-		$(answer).addClass("hide");
-		$(reviewer).removeClass("active");
 			if (id_now_number==0) {
                 id_now_number = feedback_total - 1;
             } else {
                 id_now_number -= 1;
             }
-			$('.feedback-set.show .feedback')[id_now_number].removeClass("hide");
-			$('.feedback-set.show .feedback')[id_now_number].addClass("show");
-			$('.feedback-set.show .feedback')[id_now_number].addClass("active");
 		}
 		else if (e.keyCode == 39) { //right
-		$(answer).removeClass("show");
-		$(answer).addClass("hide");
-		$(reviewer).removeClass("active");
 			if (id_now_number==feedback_total) {
-				$("#feedback-1").removeClass("hide");
-				$("#feedback-1").addClass("show");
-				$("#reviewer-1").addClass("active");
-			} else {
-				var id_next_number = id_now_number + 1;
-				$("#feedback-"+id_next_number).removeClass("hide");
-				$("#feedback-"+id_next_number).addClass("show");
-				$("#reviewer-"+id_next_number).addClass("active");
-			}
+                id_now_number = 0;
+            } else {
+                id_now_number += 1;
+            }
 		}
+
+        $('.feedback-set.show .feedback')[id_now_number].removeClass("hide");
+        $('.feedback-set.show .feedback')[id_now_number].addClass("show");
+        $('.feedback-set.show .reviewer')[id_now_number].addClass("active");
+
 	});
 });
